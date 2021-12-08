968adf941ac24da88c733c9b0e448dfa075f7c37
Danielle Fortin <fortind6@gmail.com>
2021-12-08 11:01:45 -0500

    updated arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,8 @@
 // user input variables
-var confirmNumber = [0,1,2,3,4,5,6,7,8,9];
-var confirmLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var confirmUpper = ["A","B","C","D","E","F","G","H","J","I","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var confirmSpecial = array("!", "@", "#", "$", "%", "&", "*", "(", ")", "-", "_", "+", "=","`", "~",";",":","<",">","?","/","\","|"");
+const confirmNumber = [0,1,2,3,4,5,6,7,8,9];
+const confirmLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const confirmUpper = ["A","B","C","D","E","F","G","H","J","I","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const confirmSpecial = array("!", "@", "#", "$", "%", "&", "*", "(", ")", "-", "_", "+", "=","`", "~",";",":","<",">","?","/","\","|"");
 
 var passwordValue = '';
 
